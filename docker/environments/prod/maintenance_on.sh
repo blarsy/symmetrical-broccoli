6ee6e5b29f1218728ab89ac4372174a2b3b5fe3d
blarsy <bertrand.larsy@gmail.com>
2025-06-11 17:40:50 +0200

    Primitive, but functional maintenance mode

---
+++ docker/environments/prod/maintenance_on.sh
@@ -0,0 +1,5 @@
+cd /home/symbro_test/docker/environments/test/
+docker compose down
+cd maintenance
+docker compose build --no-cache
+docker compose up -d
---
+++ docker/environments/test/maintenance_off.sh
@@ -0,0 +1,4 @@
+cd /home/symbro_test/docker/environments/test/maintenance
+docker compose down
+cd ..
+docker compose up -d
